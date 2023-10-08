a4e15817ecdae8c5a65bb81d12e7c07ab6287214
nikode33 <147321096+nikode33@users.noreply.github.com>
2023-10-08 17:45:07 -0400

    Update sketch.js

--- particle-life/sketch.js
+++ particle-life/sketch.js
@@ -1,8 +1,8 @@
-const cells = [];
+let cells = [];
 let arr = [];
 let arrp = [];
 let atoms = 700;
-let colors = 10;
+let colors = 6;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i=0; i<atoms; i++) {
@@ -21,6 +21,18 @@ function setup() {
   cradius.position(10, 50);
   cradius.style('width', '80px');
   
+  friction = createSlider(0, 100, 1);
+  friction.position(10, 70);
+  friction.style('width', '80px');
+  
+  types = createSlider(0, 100, 1);
+  types.position(10, 90);
+  types.style('width', '80px');
+  
+  satoms = createSlider(0, 5000, 1);
+  satoms.position(10, 110);
+  satoms.style('width', '80px');
+  
   for (let j=0; j<colors; j++) {
     arrp.push(random(-1, 1));
   }
@@ -32,8 +44,10 @@ function setup() {
     }
   }
   button = createButton('Randomize Attraction Values');
-  button.position(10, 80);
+  button.position(10, 140);
   button.mousePressed(randomarr);
+  satoms.changed(start);
+  types.changed(start);
 }
 
 function randomarr() {
@@ -50,12 +64,24 @@ function randomarr() {
     }
   }
 }
+
+function start() {
+  atoms = satoms.value()
+  colors = types.value()
+  cells = [];
+  for (let i=0; i<atoms; i++) {
+       cells.push([random(0, width), random(0, height), round(random(0, colors-1)), 0, 0])
+  }
+}
 function draw() {
-  background(220);
-  fill(0);
+  background(0);
+  fill(255);
   text("Attraction Radius: " + radius.value(), 100, 25);
   text("Random Movement Amount: " + move.value(), 100, 45);
   text("Cell Radius: " + cradius.value(), 100, 65);
+  text("Friction: " + friction.value()/100, 100, 85);
+  text("Particles: " + satoms.value(), 100, 125);
+  text("Colors: " + types.value(), 100, 105);
   for (let i=0; i<atoms; i++) {
     cells[i][3] += random(-move.value(), move.value());
     cells[i][4] += random(-move.value(), move.value());
@@ -78,42 +104,17 @@ function draw() {
       if (cells[i][1] <= 0 || cells[i][1] >= height) {
         cells[i][4] *= -2
       }
-      
+      if (random(0, 1) < 0.0001) {
+        cells[i][3] += random(-20, 20);
+        cells[i][4] += random(-20, 20);
+      }
+      cells[i][3] *= 1-(friction.value()/100);
+      cells[i][4] *= 1-(friction.value()/100);
       cells[i][0] += cells[i][3]
       cells[i][1] += cells[i][4]
     }
-    switch (cells[i][2]) {
-      case 0:
-        fill(255, 0, 0);
-        break;
-      case 1:
-        fill(0, 0, 255);
-        break;
-      case 2:
-        fill(0, 255, 0);
-        break;
-      case 3:
-        fill(255, 0, 255);
-        break;
-      case 4:
-        fill(255, 255, 0);
-        break;
-      case 5:
-        fill(0, 255, 255);
-        break;
-      case 6:
-        fill(127, 0, 255);
-        break;
-      case 7:
-        fill(255, 127, 127);
-        break;
-      case 8:
-        fill(0, 127, 127);
-        break;
-      case 9:
-        fill(0, 0, 0);
-        break;
-    }
+    colorMode(HSB, 255);
+    fill(map(cells[i][2], 0, colors, 0, 255), 175, 255);
     circle(cells[i][0], cells[i][1], cradius.value());
   }
 }
